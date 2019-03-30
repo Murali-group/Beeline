6d6f2ab48059695cd7383abdb72fcb888b9b1a1c
Aditya Pratapa <adyprat@csb.cs.vt.edu>
2019-03-30 22:09:44 +0000

    added specific versions for each of the medthods.

--- Algorithms/PIDC/installPackages.jl
+++ Algorithms/PIDC/installPackages.jl
@@ -1,12 +1,17 @@
 using Pkg
 Pkg.resolve()
-Pkg.add("InformationMeasures")
-Pkg.add("PyPlot")
-Pkg.add("LightGraphs")
-Pkg.add("GraphPlot")
-Pkg.add("NetworkInference")
 
-# Hopefully this precompiles the libraries
+Pkg.add(PackageSpec(name="InformationMeasures", version = "0.3.0"))
+
+Pkg.add(PackageSpec(name="PyPlot", version = "2.8.0"))
+
+Pkg.add(PackageSpec(name="LightGraphs", version = "1.2.0"))
+           
+Pkg.add(PackageSpec(name = "GraphPlot", version = "0.3.1"))
+           
+Pkg.add(PackageSpec(name = "NetworkInference", version = "0.1.0"))
+
+# Hopefully this step precompiles the libraries
 using NetworkInference
 using LightGraphs
 
