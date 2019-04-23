dd07a93b05a911fd533a64436b8bfe6fa57cfeae
jlaw9 <jeffreynlaw@gmail.com>
2019-04-23 18:32:03 -0400

    Added parameters to SCINGE

--- src/scingeRunner.py
+++ src/scingeRunner.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 import pandas as pd
 from pathlib import Path
 import numpy as np
@@ -22,24 +23,51 @@ def generateInputs(RunnerObj):
         newExpressionData['PseudoTime'] = PTData['PseudoTime']
         newExpressionData.to_csv(RunnerObj.inputDir.joinpath("SCINGE/ExpressionData.csv"),
                              sep = ',', header  = True, index = False)
-    
+
+
 def run(RunnerObj):
     '''
     Function to run SCINGE algorithm
     '''
     inputPath = "data/" + str(RunnerObj.inputDir).split("RNMethods/")[1] + \
                     "/SCINGE/ExpressionData.csv"
-    
+
     # make output dirs if they do not exist:
     outDir = "outputs/"+str(RunnerObj.inputDir).split("inputs/")[1]+"/SCINGE/"
     os.makedirs(outDir, exist_ok = True)
-    
-    outPath = "data/" +  str(outDir) 
-    cmdToRun = ' '.join(['docker run --rm -v', str(Path.cwd())+':/runSCINGE/data/ scinge:base /bin/sh -c \"./runSCINGE ', 
-                         inputPath, outPath, '\"'])
-    print(cmdToRun)
-    os.system(cmdToRun)
 
+    # if the parameters aren't specified, then use default parameters
+    # TODO allow passing in multiple sets of hyperparameters
+    # these must be in the right order!
+    params_order = [
+        'lambda', 'dT', 'num_lags', 'kernel_width',
+        'prob_zero_removal', 'prob_remove_samples',
+        'family', 'num_replicates',
+    ]
+    default_params = {
+        'lambda': '0.01',
+        'dT': '10',
+        'num_lags': '5',
+        'kernel_width': '4',
+        'prob_zero_removal': '0',
+        'prob_remove_samples': '0.2',
+        'family': 'gaussian',
+        'num_replicates': '2',
+    }
+    params = RunnerObj.params
+    for param, val in default_params.items():
+        if param not in params:
+            params[param] = val
+    params_str = ' '.join(str(params[p]) for p in params_order) 
+
+    outPath = "data/" + str(outDir) 
+    cmdToRun = ' '.join(['docker run --rm -v', str(Path.cwd())+':/runSCINGE/data/ scinge:base /bin/sh -c \"./runSCINGE ', 
+                         inputPath, outPath, params_str, '\"'])
+    print(cmdToRun)
+    # also print the parameters
+    print("\tParameters: %s" % (', '.join("%s: %s" % (p, str(params[p])) for p in params_order)))
+    #os.system(cmdToRun)
+    subprocess.check_call(cmdToRun, shell=True)
 
 
 def parseOutput(RunnerObj):
