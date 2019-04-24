2b0c513a06f9e1643d64b711100d8618807c5bae
Aditya Pratapa <adyprat@csb.cs.vt.edu>
2019-04-24 19:50:24 +0000

    Added Ensemble version 0

--- src/plotCurves.py
+++ src/plotCurves.py
@@ -20,11 +20,42 @@ def EvalCurves(dataDict, inputSettings):
                                 '/' +dataDict['trueEdges'],
                                 sep = ',', 
                                 header = 0, index_col = None)
+    
     TrueEdgeDict = {'|'.join(p):0 for p in list(product(np.unique(trueEdgesDF.loc[:,['Gene1','Gene2']]),repeat =2))}
     for key in TrueEdgeDict.keys():
         if len(trueEdgesDF.loc[(trueEdgesDF['Gene1'] == key.split('|')[0]) &
                            (trueEdgesDF['Gene2'] == key.split('|')[1])])>0:
             TrueEdgeDict[key] = 1
+            
+    outDir = "outputs/"+str(inputSettings.datadir).split("inputs/")[1]+ '/' +dataDict['name']
+    
+    
+    Pred = {}
+    for algo in inputSettings.algorithms:
+        if Path(outDir + '/' +algo[0]+'/rankedEdges.csv').exists():
+            Pred[algo[0]] = {'|'.join(p):0 for p in list(product(np.unique(trueEdgesDF.loc[:,['Gene1','Gene2']]),
+                                                                          repeat =2))}
+            predDF =  pd.read_csv(outDir+'/'+algo[0]+'/rankedEdges.csv',sep='\t')
+            for key in Pred[algo[0]].keys():
+                subDF = predDF.loc[(predDF['Gene1'] == key.split('|')[0]) &
+                                   (predDF['Gene2'] == key.split('|')[1])]
+                if len(subDF)>0:
+                    Pred[algo[0]][key] = np.abs(subDF.EdgeWeight.values[0])
+                        
+    ensembleDF = pd.DataFrame(Pred)
+    print(ensembleDF.head())
+
+    ensembleFinal = pd.DataFrame(ensembleDF.rank(axis = 'index', ascending = True).mean(axis= 'columns'))
+    print(ensembleFinal.head())
+
+    ensembleFinal.loc[:,'Gene1'] = [ix.split('|')[0] for ix in ensembleFinal.index]
+    ensembleFinal.loc[:,'Gene2'] = [ix.split('|')[1] for ix in ensembleFinal.index]
+    ensembleFinal.columns = ['EdgeWeight','Gene1','Gene2']
+    ensembleFinal.sort_values(by='EdgeWeight',ascending = True, inplace = True)
+    print("Writing Ensemble Model output to ",outDir)
+    ensembleFinal[['Gene1','Gene2','EdgeWeight']].to_csv(outDir+'/rankedEdges.csv', sep = '\t', index = False)            
+
+
             
     # Initialize data dictionaries
     precisionDict = {}
@@ -33,7 +64,6 @@ def EvalCurves(dataDict, inputSettings):
     TPRDict = {}
     AUPRC = {}
     AUROC = {}
-    outDir = "outputs/"+str(inputSettings.datadir).split("inputs/")[1]+ '/' +dataDict['name']
     for algo in inputSettings.algorithms:
 
         # check if the output rankedEdges file exists
@@ -68,6 +98,31 @@ def EvalCurves(dataDict, inputSettings):
             print(outDir + '/' +algo[0]+'/rankedEdges.csv', \
                   ' does not exist. Skipping...')
             
+            
+    predDF = pd.read_csv(outDir + '/rankedEdges.csv', \
+                                sep = '\t', header =  0, index_col = None)
+
+    PredEdgeDict = {'|'.join(p):0 for p in list(product(np.unique(trueEdgesDF.loc[:,['Gene1','Gene2']]),repeat =2))}
+    for key in PredEdgeDict.keys():
+        subDF = predDF.loc[(predDF['Gene1'] == key.split('|')[0]) &
+                           (predDF['Gene2'] == key.split('|')[1])]
+        if len(subDF)>0:
+            PredEdgeDict[key] = subDF.EdgeWeight.values[0]
+
+    outDF = pd.DataFrame([TrueEdgeDict,PredEdgeDict]).T
+    outDF.columns = ['TrueEdges','PredEdges']
+    fpr, tpr, thresholds = roc_curve(y_true=outDF['TrueEdges'],
+                                     y_score=outDF['PredEdges'], pos_label=1)
+
+    prec, recall, thresholds = precision_recall_curve(y_true=outDF['TrueEdges'],
+                                                      probas_pred=outDF['PredEdges'], pos_label=1)
+    precisionDict['Ensemble'] = prec
+    recallDict['Ensemble'] = recall 
+    FPRDict['Ensemble'] = fpr 
+    TPRDict['Ensemble'] = tpr
+    AUPRC['Ensemble'] = auc(recall, prec)
+    AUROC['Ensemble'] = auc(fpr, tpr)
+
     ## Make PR curves
     legendList = []
     for key in precisionDict.keys():
