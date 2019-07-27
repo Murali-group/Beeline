b8d724bdb0290e0b716b322bf9af95bc612e8ef4
Jeff Law <jeffl@vt.edu>
2019-07-27 21:58:55 +0000

    updated post-processing of files

--- src/monocleRunner.py
+++ src/monocleRunner.py
@@ -18,8 +18,8 @@ def generatePseudoTime(
     expr_file = inputDir.joinpath(exprDataFile)
     gene_file = outDir.joinpath(geneDataFile)
     cell_file = outDir.joinpath(cellDataFile)
-    pt_file = inputDir.joinpath(out_file)
-    out_file = outDir.joinpath(cellDataFile)
+    pt_file = outDir.joinpath(out_file)
+    out_file = inputDir.joinpath(out_file)
     time_file = outDir.joinpath("time.txt")
     processInputs(inputDir, outDir, expr_file, gene_file, cell_file)
 
@@ -33,10 +33,10 @@ def generatePseudoTime(
         'docker run --rm -v', str(Path.cwd())+':/data/ monocle:base ',
         '/bin/sh -c \"time -v -o', "data/" + str(time_file),
         'Rscript runMonocle.R',
-        '-e data/'+str(expr_file),
-        '-c data/'+str(cell_file),
-        '-g data/'+str(gene_file),
-        '-o data/'+str(outDir),
+        '-e data/%s'%str(expr_file),
+        '-c data/%s'%str(cell_file),
+        '-g data/%s'%str(gene_file),
+        '-o data/%s/'%str(outDir),
         '-l', str(lowerDetectionLimit), 
         '-x', expressionFamily])
     if log is True:
@@ -46,8 +46,13 @@ def generatePseudoTime(
     subprocess.check_call(cmdToRun, shell=True)
 
     # move the pseudotime file to the inputDir
-    print("moving %s to %s" % (out_file, pt_file))
-    shutil.move(out_file, pt_file)
+    #print("moving %s to %s" % (out_file, pt_file))
+    #shutil.move(out_file, pt_file)
+
+    # load the pseudotime file, extract the 
+    print("extracting the pseudotime column from %s and writing to %s" % (pt_file, out_file))
+    df = pd.read_csv(pt_file, header=0, index_col=0)
+    df['Pseudotime'].to_csv(out_file, sep=',', header=True)
 
 
 def processInputs(
@@ -75,8 +80,8 @@ def processInputs(
     genes_with_no_exp = gene_cell_counts[gene_cell_counts == 0]
     if len(cells_with_no_exp) != 0 or len(genes_with_no_exp) != 0:
         rewrite = True
-        print("\tdropping %d cells with 0 gene counts" % (len(cells_with_no_exp)))
-        print("\tdropping %d genes with 0 cell counts" % (len(genes_with_no_exp)))
+        print("\tdropping %d (/%d total) cells with 0 gene counts" % (len(cells_with_no_exp), len(edf.columns)))
+        print("\tdropping %d (/%d total) genes with 0 cell counts" % (len(genes_with_no_exp), len(edf.index)))
         # remove those columns from the dataframe
         edf.drop(cells_with_no_exp.index, axis=1, inplace=True)
         # remove those rows from the dataframe
