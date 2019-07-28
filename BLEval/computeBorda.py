626c5838d972b78d8a30bd0dfa7d05a0948436d6
Aditya Bharadwaj <adb@vt.edu>
2019-07-28 18:42:56 -0400

    added code to compute auroc and auprc scores with borda method

--- BLEval/computeBorda.py
+++ BLEval/computeBorda.py
@@ -7,6 +7,8 @@ from pathlib import Path
 import concurrent.futures
 from itertools import permutations
 from sklearn import preprocessing
+from sklearn.metrics import precision_recall_curve, roc_curve, auc
+
 
 
 def Borda(evalObject, selectedAlgorithms=None, aggregationMethod="average"):
@@ -32,8 +34,9 @@ def Borda(evalObject, selectedAlgorithms=None, aggregationMethod="average"):
     :returns:
         - None
     """
-    edges = []
+    evaluationDFs = []
     for dataset in tqdm(evalObject.input_settings.datasets):
+        edges = []
         for algorithmName, _ in tqdm(evalObject.input_settings.algorithms):
             outDir = str(evalObject.output_settings.base_dir) + \
                      str(evalObject.input_settings.datadir).split("inputs")[1] + \
@@ -47,6 +50,9 @@ def Borda(evalObject, selectedAlgorithms=None, aggregationMethod="average"):
             try:
                 df = pd.read_csv(rank_path, sep="\t", header=0, index_col=None)
                 refNetwork = pd.read_csv(refNetwork_path, header=0, index_col=None)
+                refNetwork['edge'] = refNetwork.apply(lambda x: '%s-%s' % (x.Gene1, x.Gene2), axis=1)
+                refNetwork = refNetwork[refNetwork.Gene1!=refNetwork.Gene2]
+                refNetwork['isReferenceEdge'] = 1
                 all_edges_df = pd.DataFrame(list(permutations(np.unique(refNetwork.loc[:,['Gene1','Gene2']]), r = 2)), columns=['Gene1','Gene2'])
                 ranked_edges = pd.merge(all_edges_df, df, on=['Gene1','Gene2'], how='left')
                 ranked_edges.EdgeWeight = ranked_edges.EdgeWeight.fillna(0)
@@ -60,11 +66,36 @@ def Borda(evalObject, selectedAlgorithms=None, aggregationMethod="average"):
                 print("\nSkipping Borda computation for ", algorithmName, "on path", outDir)
                 continue
         rank_df = pd.pivot_table(pd.concat(edges), values='normEdgeWeight', index='edge', columns='algo')
-        rank_df['Borda-AvgRank'] = __normalize__(rank_df.rank(ascending=True, method=aggregationMethod).mean(axis=1).values)
-        rank_df['mBorda-AvgRank'] = __normalize__(rank_df.rank(ascending=False, method=aggregationMethod).apply(lambda x: 1.0/(x*x)).mean(axis=1).values)
-        rank_df['sBorda-AvgRank'] = __normalize__(rank_df[selectedAlgorithms].rank(ascending=True, method=aggregationMethod).mean(axis=1).values)
-        rank_df['smBorda-AvgRank'] = __normalize__(rank_df[selectedAlgorithms].rank(ascending=False, method=aggregationMethod).apply(lambda x: 1.0/(x*x)).mean(axis=1).values)
+        rank_df['Borda'] = __normalize__(rank_df.rank(ascending=True, method=aggregationMethod).mean(axis=1).values)
+        rank_df['mBorda'] = __normalize__(rank_df.rank(ascending=False, method=aggregationMethod).apply(lambda x: 1.0/(x*x)).mean(axis=1).values)
+        rank_df['sBorda'] = __normalize__(rank_df[selectedAlgorithms].rank(ascending=True, method=aggregationMethod).mean(axis=1).values)
+        rank_df['smBorda'] = __normalize__(rank_df[selectedAlgorithms].rank(ascending=False, method=aggregationMethod).apply(lambda x: 1.0/(x*x)).mean(axis=1).values)
         rank_df.to_csv("%s/%s-Borda.csv" % (outDir, dataset["name"]), index=False)
+
+        refNetwork = refNetwork[["edge", "isReferenceEdge"]].set_index("edge")
+        rank_df = pd.merge(rank_df, refNetwork, how='left', on='edge').fillna(0)
+        rank_df.isReferenceEdge = rank_df.isReferenceEdge.astype(int)
+
+        evaluation_scores = []
+        for algo in rank_df.drop(['isReferenceEdge'], 1).columns:
+            y_true = rank_df['isReferenceEdge'].values
+            y_scores = rank_df[algo].values
+
+            prec, recall, thresholds = precision_recall_curve(y_true=y_true, probas_pred=y_scores, pos_label=1)
+            fpr, tpr, thresholds = roc_curve(y_true=y_true, y_score=y_scores, pos_label=1)
+            evaluation_scores.append([dataset["name"], algo, auc(recall, prec), auc(fpr, tpr)])
+
+        evaluationDFs.append(pd.DataFrame(evaluation_scores, columns=["dataset", "algorithm", "AUPRC", "AUROC"]))
+
+    outDir = str(evalObject.output_settings.base_dir) + \
+             str(evalObject.input_settings.datadir).split("inputs")[1]
+
+    auprcDF = pd.pivot_table(pd.concat(evaluationDFs), values='AUPRC', index='algorithm', columns='dataset')
+    auprcDF.to_csv("%s/%s-AUPRC-with-Borda.csv" % (outDir, dataset["name"].split("_")[0]), index=False)
+
+    aurocDF = pd.pivot_table(pd.concat(evaluationDFs), values='AUROC', index='algorithm', columns='dataset')
+    aurocDF.to_csv("%s/%s-AUROC-with-Borda.csv" % (outDir, dataset["name"].split("_")[0]), index=False)
+
 
 def __normalize__(arr):
     return 1.0*(arr-np.min(arr))/(np.max(arr)-np.min(arr))
