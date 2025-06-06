2cbaff4159f3822aeef3db20791412e7218ac1e4
yutuyt01 <timwilson@vt.edu>
2025-06-06 18:08:57 +0000

    moving to gpu commit

--- BLRun/testingScriptTim.py
+++ BLRun/testingScriptTim.py
@@ -4,5 +4,4 @@ import pandas as pd
 store = pd.HDFStore('rank_total_gene_rpkm.h5')
 # store = pd.HDFStore('bone_marrow_cell.h5')
 df = pd.read_hdf(store).reset_index()
-print(df)
-print(store['rpkm'])
+print(df.shape)
